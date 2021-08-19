79b0166bdafb192cfd23c5d2dcf37dad5b153fa1
BenjaCimatti <benja.cimatti@gmail.com>
2021-08-19 19:37:57 -0300

    Procfile Fix

--- random_app/views.py
+++ random_app/views.py
@@ -1,9 +1,24 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import random
+import os
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
 
 @api_view(['GET'])
 def get_random_number(request):
     random_number = random.randint(0,100)
 
     return Response(random_number)
+
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
